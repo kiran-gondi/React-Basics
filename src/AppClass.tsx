7af6540ea7ee1c088c9faf4a02c323d11b429904
kiran-gondi <kiran.gondi@gmail.com>
2024-10-06 14:46:33 +0530

    React - Components

---
+++ src/AppClass.tsx
@@ -0,0 +1,13 @@
+/* 
+Class Components are defind using JS classes.
+They are stateful components by using the lifecycle methods.
+The render method in a class component is responsible for returning JSX.
+*/
+import {Component} from 'react';
+
+class AppClass extends Component{
+    render(){
+        return <h1>Happy Hunting!</h1>;
+    }
+}
+export default AppClass; 
